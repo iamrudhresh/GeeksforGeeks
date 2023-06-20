e9bb23bb4edd7ec511f17c93f1c1c71d3cd92457
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2023-06-20 21:10:00 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Matchsticks game/matchsticks-game.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int matchGame(long long N) {
+        // code here
+        for(int i=0; i<5; i++){
+            if(N%5==0){
+                return -1;
+            }else{
+                return N%5;
+            }
+        }
+
+
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.matchGame(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
