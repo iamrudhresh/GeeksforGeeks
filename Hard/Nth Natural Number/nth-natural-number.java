48ea82ca4f30623185a9189c84ac54a349ec6560
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2023-07-01 12:09:00 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Nth Natural Number/nth-natural-number.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner ob=new Scanner(System.in);
+		int t=ob.nextInt();
+		while(t-->0)
+		{
+		    long n=ob.nextLong();
+		    Solution ab=new Solution();
+		    long k=ab.findNth(n);
+		    System.out.println(k);
+		}
+	}
+}
+
+    
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    long findNth(long N)
+   {
+       //code here
+       String str = "";
+       while(N!=0){
+           str = (N % 9) + str;
+           N = N / 9;
+       }
+       
+       return Long.parseLong(str);
+   }
+}
