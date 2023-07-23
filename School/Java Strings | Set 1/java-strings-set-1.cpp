7fd9af5d6b9c6af71b88a7ece2c8f6e483b9b663
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2023-07-24 00:58:16 +0530

    Added Solution - GfG to GitHub

---
+++ School/Java Strings | Set 1/java-strings-set-1.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string conRevstr(string S1, string S2) {
+        // code here
+        string str=S1+S2;
+        reverse(str.begin(),str.end());
+        return str;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S1, S2;
+        cin >> S1;
+        cin >> S2;
+        Solution ob;
+        cout << ob.conRevstr(S1, S2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
