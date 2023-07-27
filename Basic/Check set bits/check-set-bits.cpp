368f673b92660d5ae1a152106eab30bb60e9754c
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2023-07-28 00:14:22 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Check set bits/check-set-bits.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+   
+class Solution{
+public:
+    int isBitSet(int N){
+        // code here 
+            return !( N & N+1 ) & N;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.isBitSet(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
