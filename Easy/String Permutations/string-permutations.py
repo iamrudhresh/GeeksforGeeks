bb165f760efcaef5c6623e0d9912c56d7faeaa0a
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2023-08-06 16:46:20 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/String Permutations/string-permutations.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+from itertools import permutations
+class Solution:
+    def permutation(self,s):
+        return sorted([''.join(p) for p in permutations(s)])
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    T=int(input())
+    while(T>0):
+        s=input()
+        ob=Solution()
+        ans=ob.permutation(s)
+        for i in ans:
+            print(i,end=" ")
+        print()
+        
+        T-=1
+# } Driver Code Ends
