648535e559f85dff82c27194bf21b9a894efa3ea
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2023-09-12 14:31:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Perfect Numbers/perfect-numbers.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+int isPerfectNumber(long long N) {
+        if(N==1){return 0;}
+        long long sum=1;
+        for(int i=2; i<=sqrt(N); i++){
+            if(N%i==0){
+                sum+=i+(N/i);
+            }
+        }
+        if(sum==N){return 1;}
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.isPerfectNumber(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
