60ff506d91ab4e9bb3f21d4edf9a4ba1221f06c3
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2023-09-30 19:04:54 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse a string using Stack/reverse-a-string-using-stack.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+char* reverse(char *str, int len);
+int main()
+{
+    long long int t;
+    cin>>t;
+    while(t--)
+    {
+        char str[10000];
+        cin>>str;
+        long long int len=strlen(str);
+        char *ch=reverse(str,len);
+        for(int i=0;i<len;i++)
+        {
+            cout<<ch[i];
+        }
+        cout<<endl;
+    }
+        return 0;
+}
+
+
+// } Driver Code Ends
+
+
+//return the address of the string
+char* reverse(char *S, int len)
+{
+    //code here
+    stack<char>s;
+    for(int i=0;i<len;i++){
+        s.push(S[i]);
+    }
+    int i=0;
+    while(!s.empty()){
+        S[i]=s.top();
+        s.pop();
+        i++;
+    }
+    return S;
+}
