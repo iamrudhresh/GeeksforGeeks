82fa2542ee07d0ec84e20b9474fb479b0a273b85
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2023-10-25 09:03:46 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line1[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(line1[0]);
+            int W = Integer.parseInt(line1[1]);
+            String line2[] = in.readLine().trim().split("\\s+");
+            int val[] = new int[N];
+            for(int i = 0;i < N;i++)
+                val[i] = Integer.parseInt(line2[i]);
+            String line3[] = in.readLine().trim().split("\\s+");
+            int wt[] = new int[N];
+            for(int i = 0;i < N;i++)
+                wt[i] = Integer.parseInt(line3[i]);
+                
+            Solution ob = new Solution();
+            System.out.println(ob.knapSack(N, W, val, wt));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code here
+         int[] dp = new int[W + 1]; // Create a 1D array to store the maximum value for each weight
+
+        for (int w = 1; w <= W; w++) {
+            for (int i = 0; i < N; i++) {
+                if (wt[i] <= w) {
+                    dp[w] = Math.max(dp[w], dp[w - wt[i]] + val[i]);
+                }
+            }
+        }
+
+        return dp[W];
+    }
+}
