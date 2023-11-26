64ebc180a51cd2aa9e2934f5826ba575ba1691fc
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2023-11-26 20:13:11 +0530

    Added Solution - GfG to GitHub

---
+++ School/Check String/check-string.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            String s = sc.next ();
+            if (new Sol().check (s))
+                System.out.println ("YES");
+            else
+                System.out.println ("NO");
+        }
+        
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    Boolean check (String s)
+    {
+        // your code here       
+        char ch=s.charAt(0);
+        for(int i=1;i<s.length();i++){
+            if(ch!=s.charAt(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
