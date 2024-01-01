aa140a4c890220683e8498539b916046a321580d
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2024-01-01 09:25:17 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Array Pair Sum Divisibility Problem/array-pair-sum-divisibility-problem.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s1 = br.readLine().trim();
+            String[] S1 = s1.split(" ");
+            int n = Integer.parseInt(S1[0]);
+            int k = Integer.parseInt(S1[1]);
+            String s2 = br.readLine().trim();
+            String[] S2 = s2.split(" ");
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) {
+                nums[i] = Integer.parseInt(S2[i]);
+            }
+            Solution ob = new Solution();
+            boolean ans = ob.canPair(nums, k);
+            if (ans)
+                System.out.println("True");
+            else
+                System.out.println("False");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public boolean canPair(int[] nums, int k) {
+        // Code here
+        int n = nums.length;
+        int temp[] = new int[k];
+        for(int i = 0 ;i<n;i++){
+            temp[nums[i]%k]++;
+        }
+        
+        if(temp[0]%2!=0){
+            return false;
+        }
+        int start = 1;
+        int end = k-1;
+        while(start<=end){
+            if(temp[start++]!=temp[end--]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
