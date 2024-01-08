a539e5cfc55cb2866ef76022cae5294c5a72d2a9
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2024-01-08 22:18:51 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Merge 2 sorted linked list in reverse order/merge-2-sorted-linked-list-in-reverse-order.java
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t=scanner.nextInt();
+        while(t-->0)
+        {
+            int N = scanner.nextInt();
+            int M = scanner.nextInt();
+
+            Node node1 = null;
+            Node temp1 = null;
+            for (int i = 0; i < N; i++) {
+                int value = scanner.nextInt();
+                Node newNode = new Node(value);
+                if (node1 == null) {
+                    node1 = newNode;
+                    temp1 = node1;
+                } else {
+                    temp1.next = newNode;
+                    temp1 = temp1.next;
+                }
+            }
+    
+            Node node2 = null;
+            Node temp2 = null;
+            for (int i = 0; i < M; i++) {
+                int value = scanner.nextInt();
+                Node newNode = new Node(value);
+                if (node2 == null) {
+                    node2 = newNode;
+                    temp2 = node2;
+                } else {
+                    temp2.next = newNode;
+                    temp2 = temp2.next;
+                }
+            }
+    
+            GfG gfg = new GfG();
+            Node result = gfg.mergeResult(node1, node2);
+    
+            printList(result);
+        }
+    }
+
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+/* Structure of the node*/
+/* class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+} */
+
+class GfG
+{
+    Node mergeResult(Node node1, Node node2)
+    {
+    // Your code here
+       Node head=null,temp1=node1,temp2=node2;
+       ArrayList<Integer> al = new ArrayList<>();
+       // inserting all the elements inside the arraylist
+       while(temp1!=null){
+           al.add(temp1.data);
+           temp1=temp1.next;
+       }
+       
+       // inserting all the elements inside the arraylist
+       while(temp2!=null){
+           al.add(temp2.data);
+           temp2=temp2.next;
+       }
+       
+       //sorting arraylist in reverse order
+       Collections.sort(al, Collections.reverseOrder()); 
+       
+       if(al.size()>0){
+       Node node = new Node(al.get(0));
+       head=node;
+       for(int i=1;i<al.size();i++){
+          node.next = new Node(al.get(i));
+          node =node.next;
+       }
+      }
+       return head;
+    }
+}
+
+
