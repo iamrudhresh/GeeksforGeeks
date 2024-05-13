83599cc94ee7b3cf08db09326991027b0a26dab8
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2024-05-13 15:46:41 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Introduction to Trees/introduction-to-trees.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+
+            Solution obj = new Solution();
+            int ans = obj.countNodes(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int countNodes(int i) {
+        // code here
+        return (int) Math.pow(2,i-1);
+    }
+}
