b3f7211fbde20dfff848073731f86feb6e2577b9
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2024-05-19 02:12:51 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Frequencies of Limited Range Array Elements/frequencies-of-limited-range-array-elements.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+        int t = Integer.parseInt(br.readLine().trim()); 
+        while(t-->0){
+            
+            //size of array
+            int N = Integer.parseInt(br.readLine().trim()); 
+            int arr[] = new int[N];
+            String inputLine[] = br.readLine().trim().split(" ");
+
+            //adding elements to the array
+            for(int i = 0 ; i < N; i++){
+                arr[i]=Integer.parseInt(inputLine[i]); 
+            }
+            int P= Integer.parseInt(br.readLine().trim());
+            //calling frequncycount() function
+            Solution ob = new Solution();
+            ob.frequencyCount(arr, N, P); 
+            
+            //printing array elements
+            for(int i = 0; i < N ; i++)
+                System.out.print(arr[i] + " " );
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to count the frequency of all elements from 1 to N in the array.
+    public static void frequencyCount(int arr[], int N, int P)
+    {
+         HashMap<Integer,Integer> countMap = new HashMap<Integer,Integer>();
+        for(int n:arr){
+            if(countMap.get(n)!=null)
+                countMap.put(n,countMap.get(n)+1);
+            else
+                countMap.put(n,1);
+            
+        }
+        for(int i=1;i<=N;i++){
+            if(countMap.get(i)!=null)
+            arr[i-1] = countMap.get(i);
+            else
+            arr[i-1]=0;
+        // code here
+        }
+    }
+}
