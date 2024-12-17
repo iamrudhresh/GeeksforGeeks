6036030705f678991e53a1010ac94c2c830205f5
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2024-12-17 21:41:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution sln = new Solution();
+            int ans = sln.aggressiveCows(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public static int aggressiveCows(int[] stalls, int k) {
+        // code here
+          int n = stalls.length; //size of array
+        //sort the stalls[]:
+        Arrays.sort(stalls);
+
+        int low = 1, high = stalls[n - 1] - stalls[0];
+        //apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (placeCow(stalls, mid, k) == true) {
+                low = mid + 1;
+            } else high = mid - 1;
+        }
+        return high;
+    }
+    private static boolean placeCow(int[] stalls, int dist, int cows) {
+        int n = stalls.length; //size of array
+        int cntCows = 1; //no. of cows placed
+        int last = stalls[0]; //position of last placed cow.
+        for (int i = 1; i < n; i++) {
+            if (stalls[i] - last >= dist) {
+                cntCows++; //place next cow.
+                last = stalls[i]; //update the last location.
+            }
+            if (cntCows >= cows) return true;
+        }
+        return false;
+    }
+}
