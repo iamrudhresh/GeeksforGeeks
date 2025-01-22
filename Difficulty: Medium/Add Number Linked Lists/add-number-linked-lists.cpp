c9ff1c18fd1e552f18979b3c73dc6ea81916c1e5
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2025-01-22 21:09:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.cpp
+++ Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.cpp
@@ -53,90 +53,97 @@ void printList(Node* n) {
 
 
 // } Driver Code Ends
+/* node for linked list:
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
 class Solution {
   public:
-    // Function to reverse the linked list
-    Node* reverse(Node* head) {
-        // Initialize the previous node as NULL
-        Node* prev = NULL;
-        // Start with the current node as the head
-        Node* current = head;
-        // Variable to store the next node temporarily
-        Node* next;
-
-        // Traverse the list until the current node becomes NULL
-        while (current != NULL) {
-            // Store the next node
-            next = current->next;
-            // Reverse the pointer of the current node
-            current->next = prev;
-            // Move the previous node to the current node
-            prev = current;
-            // Move to the next node in the list
-            current = next;
-        }
-
-        // Return the new head of the reversed list (the last non-NULL node)
-        return prev;
-    }
-
-    // Function to add two numbers represented as linked lists
+   Node* reverse(Node*head){
+      Node*prev=NULL;
+      Node*next;
+      Node*curr=head;
+      while(curr!=NULL){
+          next=curr->next;
+          curr->next=prev;
+          prev=curr;
+          curr=next;
+      }
+      return prev;
+  }
     Node* addTwoLists(Node* num1, Node* num2) {
-        // Reverse both linked lists to add the numbers from the least significant digit
-        num1 = reverse(num1);
-        num2 = reverse(num2);
-
-        // Initialize the resulting sum list as NULL
-        Node* sum = NULL;
-        // Variable to store the carry from the previous digit addition
-        int carry = 0;
-
-        // Loop until both lists are exhausted or there's a remaining carry
-        while (num1 != NULL || num2 != NULL || carry != 0) {
-            // Start with the carry value
-            int newVal = carry;
-
-            // Add the value of num1's current node, if it exists
-            if (num1) {
-                newVal += num1->data;
-                // Move to the next node in num1
-                num1 = num1->next;
+        while(num1->data==0){
+            num1=num1->next;
+        }
+         while(num2->data==0){
+            num2=num2->next;
+        }
+        
+        Node*n1=reverse(num1);
+        Node*n2=reverse(num2);
+        Node*temp=new Node(-1);
+        Node*sum=temp;
+        int carry=0;
+        while(n1!=NULL&&n2!=NULL){
+            int a=n1->data;
+            int b=n2->data;
+            int digit=a+b+carry;
+            carry=digit/10;
+           int value=digit%10;
+            Node*newNode=new Node(value);
+            temp->next=newNode;
+            temp=newNode;
+            n1=n1->next;
+            n2=n2->next;
+        }
+        while(n1!=NULL){
+            int a=n1->data;
+            int digit=a+carry;
+             carry=digit/10;
+          int  value=digit%10;
+           
+             Node*newNode=new Node(value);  
+               temp->next=newNode;
+               temp=newNode;
+            
+            n1=n1->next;
+        }
+            
+        
+        while(n2!=NULL){
+            int b=n2->data;
+            int digit=b+carry;
+             carry=digit/10;
+           int value=digit%10;
+           
+             Node*newNode=new Node(value);  
+             temp->next=newNode;
+            temp=newNode;
+            n2=n2->next;
+           
+            
+        }
+        while(carry!=0){
+            int digit=carry%10;
+            if(digit==0){
+                break;
             }
-            // Add the value of num2's current node, if it exists
-            if (num2) {
-                newVal += num2->data;
-                // Move to the next node in num2
-                num2 = num2->next;
-            }
-
-            // Update the carry (the quotient of newVal divided by 10)
-            carry = newVal / 10;
-            // Get the current digit to be placed in the sum (the remainder of newVal
-            // divided by 10)
-            newVal = newVal % 10;
-
-            // Create a new node for the current digit and insert it at the beginning of
-            // the sum list
-            Node* newNode = new Node(newVal);
-            newNode->next = sum;
-            sum = newNode;
+            carry=carry/10;
+            Node*newNode=new Node(digit);
+            temp->next=newNode;
+            temp=newNode;
         }
-
-        // Remove any leading zeros from the result (if applicable)
-        while (sum != NULL && sum->data == 0) {
-            // If the current node is 0, delete it and move to the next node
-            Node* temp = sum;
-            sum = sum->next;
-            delete temp;
-        }
-
-        // If the sum list is empty after removing leading zeros, return a node with
-        // value 0
-        if (sum == NULL) {
-            return new Node(0);
-        }
-        // Return the head of the sum list
-        return sum;
+        Node*ans=reverse(sum->next);
+        return ans;
     }
 };
 
