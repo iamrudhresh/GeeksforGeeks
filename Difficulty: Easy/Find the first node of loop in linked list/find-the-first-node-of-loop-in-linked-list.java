5df50324c34c0aa2d9608c423119ec0a170589b6
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2025-01-25 22:11:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the first node of loop in linked list/find-the-first-node-of-loop-in-linked-list.java
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class LinkedList {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void makeLoop(Node head, Node tail, int x) {
+        if (x == 0) return;
+
+        Node curr = head;
+        for (int i = 1; i < x; i++) curr = curr.next;
+
+        tail.next = curr;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            int k = Integer.parseInt(br.readLine());
+            Node head = new Node(arr.get(0));
+            Node tail = head;
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
+                tail = tail.next;
+            }
+            makeLoop(head, tail, k);
+            Solution ob = new Solution();
+            Node ans = ob.findFirstNode(head);
+            System.out.println(ans == null ? -1 : ans.data);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/* class Node
+{
+    int data;
+    Node next;
+
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public static Node findFirstNode(Node head) {
+        // code here
+          
+       Set<Node> set =new HashSet<>();
+       Node current = head;
+       
+       while(current !=null ){
+           if(!set.contains(current)){
+               set.add(current);
+           }else{
+               return current;
+           }
+           current = current.next;
+       }
+       
+       return null;
+    }
+}
