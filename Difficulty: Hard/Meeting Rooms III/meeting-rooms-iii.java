1a2c62ce3f7a1283fbafb73e944ba15f561d453d
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2025-05-12 13:40:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Meeting Rooms III/meeting-rooms-iii.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); // Number of test cases
+        while (t-- > 0) {
+            int n = sc.nextInt(); // Number of rooms
+            int m = sc.nextInt(); // Number of meetings
+            int[][] meetings = new int[m][2];
+            for (int i = 0; i < m; i++) {
+                meetings[i][0] = sc.nextInt(); // Start time
+                meetings[i][1] = sc.nextInt(); // End time
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.mostBooked(n, meetings));
+            System.out.println("~");
+        }
+        sc.close();
+    }
+}
+
+
+// } Driver Code Ends
+
+// User function Template for Java
+class Solution {
+    public int mostBooked(int n, int[][] meetings) {
+        // code here
+        
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
