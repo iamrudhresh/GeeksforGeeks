0fbc1bc25cb351eb9e71c729b01b6975de558d26
RUDHRESH S <118143270+iamrudhresh@users.noreply.github.com>
2025-06-09 08:10:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/BST with Dead End/bst-with-dead-end.java
@@ -0,0 +1,16 @@
+/*
+class Node {
+        int data;
+        Node left, right;
+
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
+}*/
+
+class Solution {
+    public boolean isDeadEnd(Node root) {
+        // Code here.
+    }
+}
